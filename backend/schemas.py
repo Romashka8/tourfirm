eb4c4bdea8c2f6f4fef13d7d6fe9e63ad2cc3f35
Roman Seleznyov <63647042+Romashka8@users.noreply.github.com>
2023-10-30 19:34:12 +0300

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,17 +1,94 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, conint
+from typing import Optional
 
 
 class OperatorBase(BaseModel):
     login: str
-    password: str
 
 
 class OperatorCreate(OperatorBase):
-    pass
+    password: str
 
 
-class Operator(OperatorBase):
+class OperatorUpdate(OperatorBase):
+    id: int
+    login: Optional[str] = ""
+    password: Optional[str] = ""
+
+
+class OperatorDelete(BaseModel):
+    id: int
+
+
+class OperatorPrivate(OperatorBase):
+    id: int
+    login: str = ""
+    password: str = ""
+    # поле для туров?
+
+    class Config:
+        from_attributes = True
+
+
+class OperatorPublic(OperatorBase):
+    id: int
+
+    # поле для туров?
+
+    class Config:
+        from_attributes = True
+
+
+# HotelRoom.
+class HotelRoomBase(BaseModel):
+    is_free: Optional[bool] = True
+
+
+class HotelRoomCreate(HotelRoomBase):
+    hotel_id: int
+    places: int
+    price_for_day: float
+
+
+class HotelRoom(HotelRoomBase):
     id: int
 
     class Config:
         from_attributes = True
+
+
+# Hotel.
+class HotelBase(BaseModel):
+    name: str
+
+
+class HotelCreate(HotelBase):
+    luxury: conint(ge=1, le=5)
+    countryCode: str
+
+
+class HotelUpdate(HotelBase):
+    id: int
+    name: Optional[str] = ""
+    luxury: Optional[conint(ge=1, le=5)] = 0
+    countryCode: Optional[str] = ""
+    rooms: Optional[list[HotelRoom]] = []
+
+
+class HotelDelete(BaseModel):
+    id: int
+
+
+class Hotel(HotelBase):
+    id: int
+    luxury: conint(ge=1, le=5)
+    countryCode: str
+    rooms: list[HotelRoom] = []
+
+    class Config:
+        from_attributes = True
+
+
+# Tour.
+class TourBase(BaseModel):
+    pass
